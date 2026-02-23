b6822b2a6c760da6411a89bf62d3dc8ae47797d9
Jamil Frizzar <jfrizzar@protonmail.com>
2026-02-22 19:06:28 -0500

    In the middle of debugging the create cruise method.

--- src/cruise.java
+++ src/cruise.java
@@ -1,19 +1,22 @@
+import javax.swing.JOptionPane;
+
 public class cruise {
 
     private String cruiseID;
     private String cruiseRoute;
-    private static int cruiseCount;
     private double ticketCost;
     private int ticketSoldCount;
     private static int totalTicketSoldCount;
+    private static int cruiseCount;
     private static double totalEarning;
 
-    //There are two constructors: a default contructor and parameterized
-    //They are chained together so that the count is always updated
+
+    //Default constructor
     public cruise(){
         this("", "", 0.0, 0);
     };
 
+    //Parameterized constructor
     public cruise(String cruiseID, String cruiseRoute, double ticketCost, int ticketSoldCount){
         setCruiseID(cruiseID);
         setCruiseRoute(cruiseRoute);
@@ -52,52 +55,81 @@ public class cruise {
 
     //Set methods
     public void setCruiseID(String cruiseID){
-        if(!cruiseID.startsWith("J")){
-            throw new IllegalArgumentException("cruiseID must start with \"J\".");
+        if(!cruiseID.startsWith("J") || cruiseID.isEmpty() || cruiseID == null || cruiseID.length() != 5){
+            throw new IllegalArgumentException("cruiseID must start with a \"J\" and be 5 characters long. (e.g. J0001, J0002, etc.)");
         }
         this.cruiseID = cruiseID;
     }
 
     public void setCruiseRoute(String cruiseRoute){
         if(cruiseRoute.isEmpty() || cruiseRoute == null){
-            throw new IllegalArgumentException("Cruise route cannot be empty. Enter a cruise route in the format: location1 - location2.");
+            throw new IllegalArgumentException("The cruise route cannot be empty. Enter a cruise route in the format:" +
+            "location1 - location2. (e.g. Jamaica - Nassau)");
         }
         this.cruiseRoute = cruiseRoute;
     }
 
     public static void setCruiseCount(int cruiseCount){
         if(cruiseCount < 0){
-            throw new IllegalArgumentException("Cruise count cannot be less than 0.");
+            throw new IllegalArgumentException("The cruise count cannot be less than 0.");
         }
-        cruise.cruiseCount = cruiseCount;
+        try{
+            cruise.cruiseCount = cruiseCount;     
+        }catch(NumberFormatException nfe){
+            JOptionPane.showMessageDialog(null, "The cruise count must be an integer.");
+        }
     }
 
     public void setTicketCost(double ticketCost){
         if(ticketCost < 0.01){
             throw new IllegalArgumentException("The ticket cost cannot be less than $0.01.");
         }
-        this.ticketCost = ticketCost;
+        try{
+            this.ticketCost = ticketCost;           
+        }catch(NumberFormatException nfe){
+            JOptionPane.showMessageDialog(null, "The ticket cost must be a valid decimal number.");
+        }
     }
 
     public void setTicketSoldCount(int ticketSoldCount){
         if(ticketSoldCount < 0){
             throw new IllegalArgumentException("The ticket sold count cannot be less than 0.");
         }
-        this.ticketSoldCount = ticketSoldCount;
+        try{
+            this.ticketSoldCount = ticketSoldCount;
+        }catch(NumberFormatException nfe){
+            JOptionPane.showMessageDialog(null, "The ticket sold count must be a valid integer");
+        }
     }
 
     public static void setTotalTicketSoldCount(int totalTicketSoldCount){
         if(totalTicketSoldCount < 0){
             throw new IllegalArgumentException("The total ticket sold count a cannot be less than 0");
         }
-        cruise.totalTicketSoldCount = totalTicketSoldCount;
+        try{
+            cruise.totalTicketSoldCount = totalTicketSoldCount;
+        }catch(NumberFormatException nfe){
+            JOptionPane.showMessageDialog(null, "The total ticket sold count must be a valid integer.");
+        }
+
     }
 
     public static void setTotalEarning(double totalEarning){
-        cruise.totalEarning = totalEarning;
+        if(totalEarning < 0){
+            throw new IllegalArgumentException("The total earning must be positive");
+        }
+        try{
+            cruise.totalEarning = totalEarning;
+        }catch(NumberFormatException nfe){
+            JOptionPane.showMessageDialog(null,"The total earnings must be a valid decimal number");
+        }
     }
 
     public String toString(){
-        return "Cruise ID: " + getCruiseID() + " | Cruise Route: " + getCruiseRoute() + " | Ticket Cost: $" + getTicketCost() + " | Ticket Sold Count: " + getTicketSoldCount();
+        return "Cruise ID: " + getCruiseID() + " | Cruise Route: " + getCruiseRoute() + " | Ticket Cost: $" + String.format("%.2f", getTicketCost()) + " | Ticket Sold Count: " + getTicketSoldCount();
+    }
+
+    public static void decrementCruise(int cruiseTotal){
+        cruise.cruiseCount -= 1;
     }
 }
--- src/cruiseImp.java
+++ src/cruiseImp.java
@@ -5,21 +5,34 @@ public class cruiseImp{
         String option;
         int optionInteger;
         double revenue = 0;
-        final int MAX_CRUISES = 10;
+        final int MAX_CRUISES = 1;
+        final int MAX_PAX = 300;
         cruise[] cruisesArray = new cruise[MAX_CRUISES];
 
         do{
             //Get user input from the option selection menu
             option = customerOption();
-            optionInteger = Integer.parseInt(option);
+            try{
+                optionInteger = Integer.parseInt(option);
+            }catch(NumberFormatException nfe){
+                continue;
+            }
 
             //Create a cruise method is executed
             if(optionInteger == 1){
                 if(cruise.getCruiseCount() >= MAX_CRUISES){
-                    JOptionPane.showMessageDialog(null, "The maximum number of cruises has been reached. Please choose another option or delete existing cruises");
+                    JOptionPane.showMessageDialog(null, "The maximum number of cruises has been reached." +
+                    " Please choose another option or delete an existing cruise.");
                 }
-                cruise currentCruise = createCruise();
-                cruisesArray[cruise.getCruiseCount() - 1] = currentCruise;
+                else{
+                    cruise currentCruise = createCruise();
+                    if(cruise.getCruiseCount() == 0){
+                        cruisesArray[cruise.getCruiseCount()] = currentCruise;
+                    }
+                    else{
+                        cruisesArray[cruise.getCruiseCount() - 1] = currentCruise;
+                    }
+                }
             }
             
             //Search a cruise method is executed
@@ -72,32 +85,55 @@ public class cruiseImp{
 
     public static String customerOption(){
         String option;
-            option = JOptionPane.showInputDialog("~~ Welcome to the Frizzar Cruise Company ~~\nPlease one of the following options\n\n( 1 ) Create a new cruise\n( 2 ) Search for an existing cruise\n( 3 ) Remove an existing cruise\n( 4 ) Sell one of your cruise tickets\n( 5 ) Display all cruises\n\nType \"Exit\" if you are finished.");
+            option = JOptionPane.showInputDialog("~~ Welcome to the Frizzar Cruise Company ~~\n" +
+            "Please one of the following options\n\n" +
+            "(1) Create a new cruise\n" +
+            "(2) Search for an existing cruise\n" +
+            "(3) Remove an existing cruise\n" +
+            "(4) Sell one of your cruise tickets\n" +
+            "(5) Display all cruises\n\nType \"Exit\" if you are finished.");
+
             try{
                 if(option.toLowerCase().equals("exit"))
                     {
                         JOptionPane.showMessageDialog(null, "Exiting program.");
                         return "exit";
                     }
-                else if(Integer.parseInt(option) < 0 || Integer.parseInt(option) > 5)
+                else if(Integer.parseInt(option) < 1 || Integer.parseInt(option) > 5)
                     {
                         JOptionPane.showMessageDialog(null, "Enter an integer between 0 and 5.");
                         return "";
                     }
-                }catch(NumberFormatException nfe){
-                    JOptionPane.showMessageDialog(null, "Enter an integer between 0 and 5.");
-                    return "";
-                }
+            }catch(NumberFormatException nfe){
+                JOptionPane.showMessageDialog(null, "Enter an integer between 0 and 5.");
+                return "";
+            }catch(NullPointerException npe){
+                return "exit";
+            }
             return option;
     }
 
+
     //Create a cruise method
     public static cruise createCruise(){
-        cruise currentCruise = new cruise("J", "location1 - location2", 0.01, 0);
-        currentCruise.setCruiseID(JOptionPane.showInputDialog("Enter a cruise ID starting with the letter \"J\""));
-        currentCruise.setCruiseRoute(JOptionPane.showInputDialog("Enter a cruise route in the format: location1 - location2."));
-        currentCruise.setTicketCost(Double.parseDouble(JOptionPane.showInputDialog("Enter the ticket cost for the cruise")));
-        return currentCruise;
+        try{
+            cruise currentCruise = new cruise("J----", "location1 - location2", 0.01, 0);
+            currentCruise.setCruiseID(JOptionPane.showInputDialog("Enter a cruise ID starting with the letter \"J\"."));
+            currentCruise.setCruiseRoute(JOptionPane.showInputDialog("Enter a cruise route in the format: location1 - location2."));
+            currentCruise.setTicketCost(Double.parseDouble(JOptionPane.showInputDialog("Enter the ticket cost for the cruise")));
+            return currentCruise;
+        }catch(IllegalArgumentException iae){
+            cruise currentCruise = new cruise("J----", "location1 - location2", 0.01, 0);
+            cruise.setCruiseCount(cruise.getCruiseCount() - 2);
+            currentCruise = null;
+            return currentCruise;
+        }catch(NullPointerException npe){
+            cruise currentCruise = new cruise("J----", "location1 - location2", 0.01, 0);
+            cruise.setCruiseCount(cruise.getCruiseCount() - 2);
+            JOptionPane.showMessageDialog(null, "Invalid entry. Returning to main menu.");
+            currentCruise = null;
+            return currentCruise;
+        }
     }
 
     //Search for a cruise method
@@ -195,6 +231,7 @@ public class cruiseImp{
 
         if(cruise.getCruiseCount() == 0){
             JOptionPane.showMessageDialog(null, "There are no cruises currently.\n Returning to main menu");
+            return;
         }
 
         for(int i = 0; i < cruise.getCruiseCount(); i++){
